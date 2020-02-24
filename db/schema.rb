83d6429928205aef88c2fbfec6ed08b358321007
Vanessa Santo <vanessaesanto@gmail.com>
2020-02-24 17:01:05 +0000

    Create sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_24_164919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "movie", null: false
+    t.string "description"
+    t.text "neighborhood", null: false
+    t.string "address"
+    t.datetime "date", null: false
+    t.integer "capacity", null: false
+    t.decimal "price", precision: 10, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
