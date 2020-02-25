fa85aa69d8cc1fcb39e03230d70268d8eba76188
mauro pinto <mauro.marques.pinto@gmail.com>
2020-02-25 12:25:32 +0000

    add booking actions and views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,43 @@
 class BookingsController < ApplicationController
+  before_action :fetch_session, except: [:dashboard, :show]
+
+  def index
+    @bookings = Booking.all
+    # @bookings = Booking.where(published: true)
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(dose_params)
+    @booking.session = @session
+    if @booking.save
+      redirect_to session_path(@booking.session)
+    else
+      render :new
+    end
+
+    def show
+      @booking = Booking.find(params[:id])
+    end
+
+    def dashboard
+      @booking = Booking.where(user: current_user)
+    end
+
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:quantity, :total)
+  end
+
+  def fetch_session
+    @session = Session.find(params[:session_id])
+  end
+
 end
