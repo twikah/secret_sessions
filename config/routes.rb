f888a17e70857ab02f75cba996d93795f1cf6a43
Vanessa Santo <vanessaesanto@gmail.com>
2020-02-26 10:28:03 +0000

    Fix login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+
+  devise_scope :user do
+    root to: 'pages#home'
+    match '/sessions/user', to: 'devise/sessions#create', via: :post
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions do
     resources :bookings, only: [:index, :new, :create]
