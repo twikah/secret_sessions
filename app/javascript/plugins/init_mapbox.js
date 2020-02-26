e65b777af76c581927b988fd43c11445833ef940
Vanessa Santo <vanessaesanto@gmail.com>
2020-02-26 15:54:49 +0000

    fix map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,11 +12,32 @@ const buildMap = () => {
 };
 
 const addMarkersToMap = (map, markers) => {
+  // markers.forEach((marker) => {
+  //   new mapboxgl.Marker()
+  //     .setLngLat([ marker.lng, marker.lat ])
+  //     .addTo(map);
+  // });
+
   markers.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
+
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+    console.log(marker);
+
+    // Create a HTML element for your custom marker
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '25px';
+    element.style.height = '25px';
+
+    // Pass the element as an argument to the new marker
+    new mapboxgl.Marker(element)
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup)
       .addTo(map);
   });
+
 };
 
 const fitMapToMarkers = (map, markers) => {
@@ -35,5 +56,7 @@ const initMapbox = () => {
                                         mapboxgl: mapboxgl }));
   }
 };
+
+
 
 export { initMapbox };
